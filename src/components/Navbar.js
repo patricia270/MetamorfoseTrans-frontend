815b54e005538f411ade1414ce94d4ab9379a55d
patricia270 <patriciasilva.eq@gmail.com>
2021-11-28 11:27:02 -0400

    Feat(specialties): Navbar layout implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,15 @@
-import styled from "styled-components";
+import Header from "../styles/stylesNavbar";
+import { MdArrowBackIosNew, MdAdd } from "react-icons/md";
 
 function Navbar() {
     return(
         <Header>
+            <MdArrowBackIosNew size="25" />
+            <h1>Especialistas</h1>
+            <MdAdd size="30"/>
         </Header>
     );
 }
-
-const Header = styled.div`
-    height: 80px;
-    display: flex;
-    justify-content: space-between;
-    padding: 0 20px;
-    background-color: #FFFFFF;
-    align-items: center;
-`;
 
 export default Navbar;
 
---
+++ src/styles/stylesNavbar.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Header = styled.div`
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 20px;
+    background-color: #FFFFFF;
+    align-items: center;
+    h1 {
+        font-size: 25px;
+        font-weight: bold;
+    }
+`;
+
+export default Header;
